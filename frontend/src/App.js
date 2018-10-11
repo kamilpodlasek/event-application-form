cdc1cde5542259fa99976b85986d6c962872659a
Kamil Podlasek <admin@kamilpodlasek.pl>
2018-10-12 01:50:55 +0200

    Added form validation on frontend and improved its styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,115 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button, DatePicker, Input } from 'antd';
+import { Button, DatePicker, Input, Form, Row, Col } from 'antd';
+import { withFormik } from 'formik';
+import * as yup from 'yup';
 
 import { createEventRequest } from './event/actions/eventActions';
-import './App.css';
 import 'antd/dist/antd.css';
 
 const initialState = {
     firstName: '',
     lastName: '',
     email: '',
-    date: '',
+    date: null,
 };
 
+const textFieldsConfig = [
+    { label: 'First Name', name: 'firstName' },
+    { label: 'Last Name', name: 'lastName' },
+    { label: 'E-mail', name: 'email' },
+];
+
+const validationSchema = yup.object().shape({
+    firstName: yup
+        .string()
+        .trim()
+        .required('First name is required'),
+    lastName: yup
+        .string()
+        .trim()
+        .required('Last name is required'),
+    email: yup
+        .string()
+        .email('E-mail must be valid')
+        .required('E-mail is required'),
+    date: yup
+        .date()
+        .typeError('Date is required')
+        .required('Date is required'),
+});
+
+function getValidateStatuses({ touched, errors }) {
+    return Object.entries(touched).reduce((acc, [fieldName, fieldTouched]) => {
+        if (!fieldTouched) {
+            return acc;
+        }
+
+        if (errors[fieldName]) {
+            return { ...acc, [fieldName]: 'error' };
+        }
+
+        return { ...acc, [fieldName]: 'success' };
+    }, {});
+}
+
 class AppContainer extends Component {
-    state = initialState;
-
-    handleInput = e => {
-        this.setState({ [e.target.name]: e.target.value });
+    handleDateChange = dateMoment => {
+        const event = { target: { name: 'date', value: dateMoment } };
+        this.props.handleChange(event);
     };
 
-    handleDate = (_dateMoment, date) => {
-        this.setState({ date });
-    };
-
-    handleSubmit = () => {
-        this.props.createEventRequest(this.state);
-    };
+    handleDatePickerTrigger = opened => !opened && this.props.handleBlur({ target: { name: 'date' } });
 
     render() {
+        const { values, touched, errors, handleChange, handleBlur, handleSubmit } = this.props;
+        const validateStatuses = getValidateStatuses(this.props);
+
         return (
             <div className="container">
-                <Input name="firstName" value={this.state.firstName} onChange={this.handleInput} />
-                <Input name="lastName" value={this.state.lastName} onChange={this.handleInput} />
-                <Input name="email" value={this.state.email} onChange={this.handleInput} />
-                <DatePicker onChange={this.handleDate} />
-                <Button type="primary" onClick={this.handleSubmit}>
-                    Submit
-                </Button>
+                <Row>
+                    <Col span={6} offset={9}>
+                        <Form onSubmit={handleSubmit}>
+                            {textFieldsConfig.map(({ label, name }) => (
+                                <Form.Item
+                                    key={name}
+                                    label={label}
+                                    validateStatus={validateStatuses[name]}
+                                    help={touched[name] && errors[name]}
+                                    hasFeedback={!!touched[name]}
+                                >
+                                    <Input
+                                        name={name}
+                                        value={values[name]}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                    />
+                                </Form.Item>
+                            ))}
+
+                            <Form.Item
+                                label="Date"
+                                validateStatus={validateStatuses.date}
+                                help={!values.date && touched.date && errors.date}
+                                hasFeedback={!!touched.date}
+                            >
+                                <DatePicker
+                                    name="date"
+                                    value={values.date}
+                                    onChange={this.handleDateChange}
+                                    onOpenChange={this.handleDatePickerTrigger}
+                                    style={{ width: '100%' }}
+                                />
+                            </Form.Item>
+
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" block>
+                                    Submit
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </Col>
+                </Row>
             </div>
         );
     }
@@ -48,4 +120,11 @@ export const App = connect(
     {
         createEventRequest,
     },
-)(AppContainer);
+)(
+    withFormik({
+        validationSchema,
+        mapPropsToValues: () => initialState,
+        handleSubmit: ({ date, ...values }, { props }) =>
+            props.createEventRequest({ ...values, date: date.toString() }),
+    })(AppContainer),
+);
